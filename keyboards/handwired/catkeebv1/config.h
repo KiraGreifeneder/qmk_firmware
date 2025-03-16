e63925c51dbda6320a2db2f412fd0b535667ef31
Kira Greifeneder <kira.greifeneder@outlook.com>
2025-03-17 00:35:04 +0100

    catkeeb init

---
+++ keyboards/handwired/catkeebv1/config.h
@@ -0,0 +1,16 @@
+#define MATRIX_ROW_PINS_RIGHT { D0, D4, C6, D7, E6 }
+#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6, F5 }
+
+#define MATRIX_COL_PINS_LEFT { F4, B6, B2, B3, B1, F7, F6, F5 }
+#define MATRIX_ROW_PINS_LEFT { D0, D4, C6, D7, E6 }
+
+#define ENCODER_A_PINS { D2 }
+#define ENCODER_B_PINS { D3 }
+#define ENCODER_RESOLUTION 1
+#define ENCODER_MAP_KEY_DELAY 10
+
+#define MASTER_LEFT
+#define SOFT_SERIAL_PIN D1
+#define SELECT_SOFT_SERIAL_SPEED {1}
+#define SPLIT_MAX_CONNECTION_ERRORS 10 // 10 attempts until it is assumed the other half isn't connected and gives up
+#define SPLIT_LAYER_STATE_ENABLE
---
+++ keyboards/handwired/catkeebv1/keymaps/default/keymap.c
@@ -0,0 +1,57 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+// umlaut definition
+#define UML_AE RALT(KC_Q)
+#define UML_OE RALT(KC_P)
+#define UML_UE RALT(KC_Y)
+#define GER_SZ RALT(KC_S)
+#define EU_EUR RALT(KC_5)
+
+// encoder mapping
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_A, KC_B)},
+    //[1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+};
+#endif
+
+// custom mod stuff
+enum custom_keycodes {
+    SHIFT_F11 = SAFE_RANGE,
+    MULTI_MOD
+};
+
+
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case SHIFT_F11:
+        if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LEFT_SHIFT) SS_DOWN(X_F11) SS_UP(X_LEFT_SHIFT) SS_DOWN(X_F11));
+        }
+        // register the double tap of the mod key to switch to debug mode
+        break;
+    return true;
+}; */
+
+
+// TODO: Column mod keys, other half
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_ESCAPE,     KC_GRAVE,      KC_1,    KC_2,        KC_3,     KC_4, KC_5, KC_6,
+        KC_F12,     KC_TAB,        KC_Q,    KC_W,        KC_E,     KC_R, KC_T, KC_LEFT_BRACKET,
+        KC_F11,        KC_F5,      KC_A,    KC_S,        KC_D,     KC_F, KC_G, KC_MINUS,
+        KC_F10,        KC_LEFT_SHIFT, KC_Z,    KC_X,        KC_C,     KC_V, KC_B, KC_KB_MUTE, //TODO rofi mute compat
+        KC_F6,         KC_LEFT_CTRL,  KC_HOME, KC_LEFT_ALT, KC_SPACE
+
+    )/* ,
+    [1] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, EU_EUR,  _______, _______, _______,
+        _______, _______, UML_AE,  GER_SZ,  _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______
+    ) */
+};
