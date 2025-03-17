544e06c55c724649b9489a3979d57da531616c87
Kira Greifeneder <kira.greifeneder@outlook.com>
2025-03-17 01:29:41 +0100

    split config improvements

--- keyboards/handwired/catkeebv1/config.h
+++ keyboards/handwired/catkeebv1/config.h
@@ -1,8 +1,9 @@
-#define MATRIX_ROW_PINS_RIGHT { D0, D4, C6, D7, E6 }
-#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6, F5 }
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 8
 
-#define MATRIX_COL_PINS_LEFT { F4, B6, B2, B3, B1, F7, F6, F5 }
-#define MATRIX_ROW_PINS_LEFT { D0, D4, C6, D7, E6 }
+#define MATRIX_COL_PINS { F4, B6, B2, B3, B1, F7, F6, F5 }
+#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6, F5, NO_PIN }
+#define MATRIX_ROW_PINS { D0, D4, C6, D7, E6 }
 
 #define ENCODER_A_PINS { D2 }
 #define ENCODER_B_PINS { D3 }
@@ -11,6 +12,6 @@
 
 #define MASTER_LEFT
 #define SOFT_SERIAL_PIN D1
-#define SELECT_SOFT_SERIAL_SPEED {1}
+#define SELECT_SOFT_SERIAL_SPEED 5
 #define SPLIT_MAX_CONNECTION_ERRORS 10 // 10 attempts until it is assumed the other half isn't connected and gives up
 #define SPLIT_LAYER_STATE_ENABLE
--- keyboards/handwired/catkeebv1/keymaps/default/keymap.c
+++ keyboards/handwired/catkeebv1/keymaps/default/keymap.c
@@ -41,11 +41,16 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESCAPE,     KC_GRAVE,      KC_1,    KC_2,        KC_3,     KC_4, KC_5, KC_6,
-        KC_F12,     KC_TAB,        KC_Q,    KC_W,        KC_E,     KC_R, KC_T, KC_LEFT_BRACKET,
-        KC_F11,        KC_F5,      KC_A,    KC_S,        KC_D,     KC_F, KC_G, KC_MINUS,
-        KC_F10,        KC_LEFT_SHIFT, KC_Z,    KC_X,        KC_C,     KC_V, KC_B, KC_KB_MUTE, //TODO rofi mute compat
-        KC_F6,         KC_LEFT_CTRL,  KC_HOME, KC_LEFT_ALT, KC_SPACE
+        KC_F12,        KC_TAB,        KC_Q,    KC_W,        KC_E,     KC_R, KC_T, KC_LBRC,
+        KC_F11,        KC_F5,         KC_A,    KC_S,        KC_D,     KC_F, KC_G, KC_MINUS,
+        KC_F10,        KC_LEFT_SHIFT, KC_Z,    KC_X,        KC_C,     KC_V, KC_B, KC_KB_MUTE,
+        KC_F6,         KC_LEFT_CTRL,  KC_HOME, KC_LEFT_ALT, KC_SPACE,
 
+        KC_7,          KC_8,          KC_9,    KC_0,        KC_SLASH, KC_NUBS, KC_BSPC,
+        KC_RBRC,       KC_Y,          KC_U,    KC_I,        KC_O,     KC_P,    KC_QUOT,
+        KC_EQL,        KC_H,          KC_J,    KC_K,        KC_L,     KC_SCLN, KC_ENTER,
+        KC_NO,         KC_N,          KC_M,    KC_COMM,     KC_DOT,   KC_UP,   KC_RIGHT_SHIFT,
+                       KC_SPACE,      KC_LEFT, KC_DOWN,     KC_RIGHT
     )/* ,
     [1] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______,
