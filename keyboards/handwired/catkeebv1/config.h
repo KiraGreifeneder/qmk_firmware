b113d5c12b4174243074745a38c9b5064416179f
Kira Greifeneder <kira.greifeneder@outlook.com>
2025-03-19 23:56:43 +0100

    working split

--- keyboards/handwired/catkeebv1/config.h
+++ keyboards/handwired/catkeebv1/config.h
@@ -1,8 +1,8 @@
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 8
 
-#define MATRIX_COL_PINS { F4, B6, B2, B3, B1, F7, F6, F5 }
-#define MATRIX_COL_PINS_RIGHT { B6, B2, B3, B1, F7, F6, F5, NO_PIN }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS_RIGHT { NO_PIN, F5, F6, F7, B1, B3, B2, B6 }
 #define MATRIX_ROW_PINS { D0, D4, C6, D7, E6 }
 
 #define ENCODER_A_PINS { D2 }
@@ -15,9 +15,9 @@
 #define SELECT_SOFT_SERIAL_SPEED 1
 // #define SPLIT_MAX_CONNECTION_ERRORS 10 // 10 attempts until it is assumed the other half isn't connected and gives up
 #define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_USB_DETECT
+
+#define SPLIT_USB_TIMEOUT 2500 
 
 #define SERIAL_DEBUG
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) || \
-    keyboard_report->mods == (MOD_BIT(KC_SPACE) | MOD_BIT(KC_B)) \
-)
+#define IS_COMMAND() ( keyboard_report->mods == (MOD_BIT(KC_SPACE) | MOD_BIT(KC_B)) )
--- keyboards/handwired/catkeebv1/keymaps/default/keymap.c
+++ keyboards/handwired/catkeebv1/keymaps/default/keymap.c
@@ -24,26 +24,21 @@ enum custom_keycodes {
     MULTI_MOD
 };
 
-
-/* bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-    case SHIFT_F11:
-        if (record->event.pressed) {
-            SEND_STRING(SS_DOWN(X_LEFT_SHIFT) SS_DOWN(X_F11) SS_UP(X_LEFT_SHIFT) SS_DOWN(X_F11));
-        }
-        // register the double tap of the mod key to switch to debug mode
-        break;
-    return true;
-}; */
+void keyboard_post_init_user(void) {
+    // Customise these values to desired behaviour
+    debug_enable=true;
+    debug_matrix=true;
+    debug_keyboard=true;
+  }
 
 
 // TODO: Column mod keys, other half
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESCAPE,     KC_GRAVE,      KC_1,    KC_2,        KC_3,     KC_4, KC_5, KC_6,             KC_7,          KC_8,          KC_9,    KC_0,        KC_SLASH, KC_NUBS, KC_BSPC,
+        KC_ESC,     KC_GRAVE,      QK_BOOT,    KC_2,        KC_3,     KC_4, KC_5, KC_6,             KC_7,          KC_8,          KC_9,    KC_0,        KC_SLASH, KC_NUBS, KC_BSPC,
         KC_F12,        KC_TAB,        KC_Q,    KC_W,        KC_E,     KC_R, KC_T, KC_LBRC,          KC_RBRC,       KC_Y,          KC_U,    KC_I,        KC_O,     KC_P,    KC_QUOT,
         KC_F11,        KC_F5,         KC_A,    KC_S,        KC_D,     KC_F, KC_G, KC_MINUS,         KC_EQL,        KC_H,          KC_J,    KC_K,        KC_L,     KC_SCLN, KC_ENTER,
-        KC_F10,        KC_LEFT_SHIFT, KC_Z,    KC_X,        KC_C,     KC_V, KC_B, KC_KB_MUTE,       KC_NO,         KC_N,          KC_M,    KC_COMM,     KC_DOT,   KC_UP,   KC_RIGHT_SHIFT,
+        KC_F10,        KC_LEFT_SHIFT, KC_Z,    KC_X,        KC_C,     KC_V, KC_B, KC_KB_MUTE,       KC_KB_MUTE,    KC_N,          KC_M,    KC_COMM,     KC_DOT,   KC_UP,   KC_RIGHT_SHIFT,
         KC_F6,         KC_LEFT_CTRL,  KC_HOME, KC_LEFT_ALT, KC_SPACE,                               KC_SPACE,      KC_LEFT,  KC_DOWN,     KC_RIGHT
     )/* ,
     [1] = LAYOUT(
