17eadfd633d44dea2f256a8f630241071a72084f
KiraGreifeneder <kira.greifeneder@outlook.com>
2025-03-21 14:38:47 +0100

    handedness by pin

--- keyboards/handwired/catkeebv1/config.h
+++ keyboards/handwired/catkeebv1/config.h
@@ -1,4 +1,4 @@
-#define MATRIX_ROWS 10
+#define MATRIX_ROWS 10 // 10 because of the way QMK represents sides: left "above" right
 #define MATRIX_COLS 8
 
 #define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6 }
@@ -10,14 +10,18 @@
 #define ENCODER_RESOLUTION 1
 #define ENCODER_MAP_KEY_DELAY 10
 
-#define MASTER_LEFT
+// Serial through TRRS jack
 #define SOFT_SERIAL_PIN D1
 #define SELECT_SOFT_SERIAL_SPEED 1
-// #define SPLIT_MAX_CONNECTION_ERRORS 10 // 10 attempts until it is assumed the other half isn't connected and gives up
+
+#define SPLIT_HAND_PIN B4
+#define	SPLIT_HAND_PIN_LOW_IS_LEFT
+
+// share layer state across sides
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_USB_DETECT
 
 #define SPLIT_USB_TIMEOUT 2500 
 
-#define SERIAL_DEBUG
-#define IS_COMMAND() ( keyboard_report->mods == (MOD_BIT(KC_SPACE) | MOD_BIT(KC_B)) )
+// #define SERIAL_DEBUG
+// #define IS_COMMAND() ( keyboard_report->mods == (MOD_BIT(KC_SPACE) | MOD_BIT(KC_B)) )
